e4c78e5d82ee94dc2444b89e0c9ae361bc1a8f40
mathis <ecomath360@gmail.com>
2022-06-08 13:24:31 -0400

    ADD - ASStructure.java

---
+++ src/interpreteur/as/lang/datatype/ASStructure.java
@@ -0,0 +1,22 @@
+package interpreteur.as.lang.datatype;
+
+public class ASStructure implements ASObjet<Object> {
+
+    public ASStructure() {
+    }
+
+    @Override
+    public Object getValue() {
+        return null;
+    }
+
+    @Override
+    public boolean boolValue() {
+        return false;
+    }
+
+    @Override
+    public String obtenirNomType() {
+        return null;
+    }
+}
